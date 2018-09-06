16c225499a71c555801b66feb8945eb123a7c1dc
simplixcurrency <42811191+simplixcurrency@users.noreply.github.com>
2018-09-05 20:11:02 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -159,8 +159,8 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0xF3, 0x61, 0x17, 0x31, 0xC4, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-  uint32_t const GENESIS_NONCE = 10000;
+  std::string const GENESIS_TX = "";
+  uint32_t const GENESIS_NONCE = ;
 
   namespace testnet
   {
