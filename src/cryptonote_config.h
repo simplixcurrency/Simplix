f4a102cdbef0a3832834bf1331a270e53548ea71
simplixcurrency <42811191+simplixcurrency@users.noreply.github.com>
2018-12-05 20:45:38 -0500

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -173,8 +173,8 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x21 ,0x03, 0xC2, 0x64 , 0x73, 0x14 , 0x41, 0x60, 0x71, 0x13, 0x00, 0xB2, 0xC6, 0xA4, 0xD1, 0xF1
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-    uint32_t const GENESIS_NONCE = 58623987;
+    std::string const GENESIS_TX = "";
+    uint32_t const GENESIS_NONCE = ;
   }
 
   namespace stagenet
